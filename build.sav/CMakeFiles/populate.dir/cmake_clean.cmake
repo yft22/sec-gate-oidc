76968b7b3490e8a69bea2ee3f2e28eb0a354810a
Fulup Ar Foll <fulup@iot.bzh>
2021-06-10 14:35:07 +0200

    added dex IOT sample

--- build.sav/CMakeFiles/populate.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "afb-sgate-oidc-debug-test.wgt"
-  "afb-sgate-oidc-debug.wgt"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build.sav/conf.d/project/etc/CMakeFiles/oidc-config.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/oidc-config"
-  "oidc-config"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/oidc-config.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
