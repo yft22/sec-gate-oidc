fc0e8fcad524ea81eed7c31adf0ab8e2f0708d7a
vlefebvre <valentin.lefebvre@iot.bzh>
2021-07-23 12:14:41 +0200

    [PACKAGING] remove uthash required for packaging on ubuntu/debian
    Signed-off-by: vlefebvre <valentin.lefebvre@iot.bzh>

--- conf.d/cmake/00-debian-config.cmake
+++ conf.d/cmake/00-debian-config.cmake
@@ -1,5 +1,4 @@
 message(STATUS "Custom options: 00-debian-config.cmake --")
-list(APPEND PKG_REQUIRED_LIST uthash)
 
 # Libshell is not part of standard Linux Distro (https://libshell.org/)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")
